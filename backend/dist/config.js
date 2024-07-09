56e542dc290dc5c257a9f71076a9f55b4ef5b54a
Rithk <rithkchaudhary86@gmail.com>
2024-07-09 16:29:13 +0530

    final project

--- backend/dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.password = exports.DATABASE_URL = exports.JWT_SECRET = void 0;
-exports.JWT_SECRET = "mysecretkey";
-exports.DATABASE_URL = "mongodb+srv://naruto:jVSNMMGwfSVPzHXV@cluster0.78poq1d.mongodb.net/Medlr-Pharmacy";
-exports.password = "adminPassword";
